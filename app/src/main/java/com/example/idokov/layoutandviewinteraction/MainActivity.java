04b6135a70455f48ae754b2aca8e985912fec939
Ivan Dokov <ikdokov@gmail.com>
2016-05-12 16:09:44 +0300

    Influence views runtime

--- app/src/main/java/com/example/idokov/layoutandviewinteraction/MainActivity.java
+++ app/src/main/java/com/example/idokov/layoutandviewinteraction/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.idokov.layoutandviewinteraction;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 
 import com.example.idokov.layoutandviewinteraction.databinding.ActivityMainBinding;
 
@@ -15,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         MainActivityHandlers handlers = new MainActivityHandlers();
         binding.setHandler(handlers);
+
+        final RadioGroup group1 = (RadioGroup) findViewById(R.id.orientation);
+        group1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.horizontal:
+                        group.setOrientation(LinearLayout.HORIZONTAL);
+                        break;
+                    case R.id.vertical:
+                        group.setOrientation(LinearLayout.VERTICAL);
+                        break;
+                }
+            }
+        });
     }
 }
